246b0204050cafe064598a36693ebc504794f804
19980417-create <19980417@clases.edu.sv>
2026-05-20 08:25:29 -0600

    Ejercicio1
    Elabore un formulario que solicite el nombre del usuario y envíe la información a otro archivo PHP donde se muestre un mensaje de bienvenida con el nombre ingresado.

---
+++ Ejercicio1.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre = $_POST["nombre"];
+echo "<h1>Bienvenido  $nombre </h1>";
+echo "<br>";
+
+echo "<br><br>";
+echo "<a href='Ejercicio1.html'>";
+echo "<button>Volver al formulario</button>";
+echo "</a>";
+
+echo "<style>
+body{
+    background-color: gray;
+}
+</style>";
+
+echo "<br><br>";
+echo "<a href='index.php'>";
+echo "<button>Volver al index</button>";
+echo "</a>";
+
+?>
